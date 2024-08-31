4e290990c1b8ca729ea33351baf7ee7b3a6dae67
bonjorno7 <jorijndegraaf@gmail.com>
2024-08-31 21:40:00 +0200

    Create chart.gd
    - Tempo: Indicates beats per minute from a given beat, used to convert between beat and time.
    - Speed: Affects note position by stretching a region of time.
    - Zoom: Affects note position by stretching a region of space.
    - Note: Objects you have to press (and hold, if the length is greater than zero).

---
+++ scripts/chart.gd
@@ -0,0 +1,54 @@
+class_name Chart
+
+var tempos: Array[Tempo] = []
+var speeds: Array[Speed] = []
+var zooms: Array[Zoom] = []
+var notes: Array[Note] = []
+
+
+class Tempo:
+	var beat: float
+	var value: float
+
+	func _init(beat_ := 0.0, value_ := 120.0):
+		beat = beat_
+		value = value_
+
+
+class Speed:
+	var layer: int
+	var beat: float
+	var value: float
+	var curve: float
+
+	func _init(layer_ := 0, beat_ := 0.0, value_ := 1.0, curve_ := 0.0):
+		layer = layer_
+		beat = beat_
+		value = value_
+		curve = curve_
+
+
+class Zoom:
+	var layer: int
+	var beat: float
+	var value: float
+	var curve: float
+
+	func _init(layer_ := 0, beat_ := 0.0, value_ := 1.0, curve_ := 1.0):
+		layer = layer_
+		beat = beat_
+		value = value_
+		curve = curve_
+
+
+class Note:
+	var layer: int
+	var lane: int
+	var beat: float
+	var length: float
+
+	func _init(layer_ := 0, lane_ := 0, beat_ := 0.0, length_ := 0.0):
+		layer = layer_
+		lane = lane_
+		beat = beat_
+		length = length_

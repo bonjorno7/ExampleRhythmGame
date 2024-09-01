7caf3c633f0dba2484ff83d2993ae86cc0c70333
bonjorno7 <jorijndegraaf@gmail.com>
2024-09-01 02:15:28 +0200

    Use explicit type hints for Game settings
    I let the engine guess types for less important variables.
    Semi-related: If a variable has no default value that's deliberate; it's either set immediately in the constructor, or is not meant to be accessed before being set externally.

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,11 @@
 ## Autoload Game
 extends Node
 
-var fullscreen := false
-var volume := 1.0
-var audio_offset := 0.000
-var video_offset := 0.000
-var scroll_speed := 1.000
+var fullscreen: bool = false
+var volume: float = 1.0
+var audio_offset: float = 0.000
+var video_offset: float = 0.000
+var scroll_speed: float = 1.000
 
 var song: Song
 var chart: Chart

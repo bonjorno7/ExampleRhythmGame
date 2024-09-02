40f2b08d9be283187ee4c3ddf6d352c454cc56cb
bonjorno7 <jorijndegraaf@gmail.com>
2024-09-02 16:50:23 +0200

    Increase default scroll speed to 8
    Using my powers of foresight I've determined that 1 is much too slow.

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,7 +5,7 @@ var fullscreen: bool = false
 var volume: float = 0.5
 var audio_offset: float = 0.000
 var video_offset: float = 0.000
-var scroll_speed: float = 1.000
+var scroll_speed: float = 8.000
 
 var song: Song
 var chart: Chart

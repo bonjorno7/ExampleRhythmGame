e519b21fe873df4a8e63ed13bf70d53f4d3aa555
bonjorno7 <jorijndegraaf@gmail.com>
2024-09-03 15:58:18 +0200

    Remove song/chart/timing/layers from Game
    Just because taronuke's game puts it in an autoload doesn't mean I have to.
    I think it makes more sense to store them in the scenes that use them, and pass them along to objects in need.
    I'm planning on using Song and Timing in the options and drift scenes.
    For example if I wanted to put the options on top of the gameplay while it's paused, I don't want to have to overwrite the song/timing used by the gameplay.
    The neat thing about version control is that I can always undo this change if I end up regretting it.

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,29 +6,3 @@ var volume: float = 0.5
 var audio_offset: float = 0.000
 var video_offset: float = 0.000
 var scroll_speed: float = 8.000
-
-var song: Song
-var chart: Chart
-var timing: Timing
-var layers: Array[Layer]
-
-
-func _init() -> void:
-	timing = Timing.new()
-
-	for index in range(8):
-		layers.append(Layer.new(index))
-
-
-func setup() -> void:
-	timing.setup(song)
-
-	for layer in layers:
-		layer.setup(chart, timing)
-
-
-func update(time: float) -> void:
-	timing.update(time)
-
-	for layer in layers:
-		layer.update(timing.time_video)

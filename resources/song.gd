42e9c892037db631355e474da8e8b271746cd7e0
bonjorno7 <jorijndegraaf@gmail.com>
2024-09-05 01:45:27 +0200

    Change order of items in Song
    Song select stuff and gameplay things are grouped better this way.

--- resources/song.gd
+++ resources/song.gd
@@ -3,7 +3,7 @@ extends Resource
 
 @export var title: String
 @export var artist: String
+@export var audio: AudioStream
 @export var offset: float
 @export var tempos: Array[Tempo]
 @export var charts: Array[Chart]
-@export var audio: AudioStream

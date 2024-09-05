4448d9536946d7ca789bc7e2c1fff6cbbc09aeb5
bonjorno7 <jorijndegraaf@gmail.com>
2024-09-06 01:53:07 +0200

    Fix MusicPlayer pause before play
    If you paused the tree before the sound started playing, it would not handle things properly.
    I solve this by storing the time at which we paused and comparing to the time at which we unpause.
    This is only done when the sound is not playing, otherwise I still use the mix center approach.
    I'm also using it for get_time_engine while paused, instead of checking whether the audio is playing in general, because engine time needs to keep changing before (in the negative) and after (to keep the visuals moving) the audio plays.
    I can't just check whether the tree is paused, because the process mode could flip things.
    Don't use stream_paused directly, only pause by pausing the scene tree, so it can receive the pause/unpause notifications.
    For get_time_player I use not playing instead of stream_paused, because I don't want mix time affecting the output while the audio isn't playing.
    Neither of these really matter, because I'm not calling these functions while the tree is paused anyway; it's there just in case.

--- nodes/music_player.gd
+++ nodes/music_player.gd
@@ -6,6 +6,7 @@ extends AudioStreamPlayer
 
 var _play_later: bool  # Start audio playback after a given delay.
 var _time_start: float  # Used to determine time relative to playback.
+var _time_pause: float  # Used to offset start time after unpausing.
 var _offset_array: PackedFloat64Array  # Rotating array of offsets.
 var _offset_index: int  # Oldest element in the array of offsets.
 var _offset_sum: float  # Sum of offsets, used to calculate average.
@@ -13,9 +14,15 @@ var _offset_average: float  # Average offset, used to adjust time.
 
 
 func _notification(what: int) -> void:
-	if what == NOTIFICATION_UNPAUSED:
-		var mix_center := get_playback_position() + AudioServer.get_output_latency() * 0.5
-		_time_start += get_time_engine() - mix_center - get_time_offset()
+	if what == NOTIFICATION_PAUSED:
+		_time_pause = Time.get_ticks_usec() / 1_000_000.0
+
+	elif what == NOTIFICATION_UNPAUSED:
+		if playing:
+			var mix_center := get_playback_position() + AudioServer.get_output_latency() * 0.5
+			_time_start += get_time_engine() - mix_center - get_time_offset()
+		else:
+			_time_start += Time.get_ticks_usec() / 1_000_000.0 - _time_pause
 
 
 func _physics_process(_delta: float) -> void:
@@ -34,8 +41,9 @@ func _physics_process(_delta: float) -> void:
 
 ## Start playing at a given time, negative numbers start after a delay.
 func start(time: float = 0.0) -> void:
+	_time_pause = Time.get_ticks_usec() / 1_000_000.0
+	_time_start = _time_pause - time
 	_play_later = time < 0.0
-	_time_start = Time.get_ticks_usec() / 1_000_000.0 - time
 
 	_offset_array.clear()
 	_offset_array.resize(roundi(buffer * Engine.physics_ticks_per_second))
@@ -50,7 +58,7 @@ func start(time: float = 0.0) -> void:
 ## Time relative to playback start.
 func get_time_engine() -> float:
 	if stream_paused:
-		return get_time_player() + get_time_offset()
+		return _time_pause - _time_start
 
 	return Time.get_ticks_usec() / 1_000_000.0 - _time_start
 
